cff3107909bb6e1f4d3cb0422e3a4c8d5437aab5
Im-Rises <quentin-morel88@hotmail.com>
2023-08-18 17:29:32 -0400

    Added project base class

---
+++ GalaxySimulator/GalaxySimulatorLauncher.h
@@ -0,0 +1,9 @@
+#ifndef GALAXYSIMULATORLAUNCHER_H
+#define GALAXYSIMULATORLAUNCHER_H
+
+class GalaxySimulatorLauncher {
+public:
+    static auto start() -> int;
+};
+
+#endif // GALAXYSIMULATORLAUNCHER_H
---
+++ GalaxySimulator/main.cpp
@@ -0,0 +1,6 @@
+#include "GalaxySimulatorLauncher.h"
+
+int main(int argc, char* argv[]) {
+    GalaxySimulatorLauncher launcher;
+    return launcher.start();
+}
